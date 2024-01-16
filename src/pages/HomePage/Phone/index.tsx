6e35e04aa56bdd2b3f0ca643b143c8fd04d1ec9d
Adriana <97173869+a-zajonc@users.noreply.github.com>
2024-01-16 22:02:19 +0100

    Add Rendering Users Name nad Email, Add Border to Avatar

--- src/pages/HomePage/Phone/index.tsx
+++ src/pages/HomePage/Phone/index.tsx
@@ -2,6 +2,8 @@ import styles from "./index.module.scss";
 import { ReactComponent as ImagePhone } from "../../../images/illustration-phone-mockup.svg";
 import { MediaBox } from "../../../components/MediaBox";
 import { useUserContext, useUserLinkData } from "../../../context";
+import { Text } from "../../../components/Text";
+import { Stack } from "../../../components/Stack";
 
 export function Phone() {
   const { userLinkData } = useUserLinkData();
@@ -11,9 +13,21 @@ export function Phone() {
     <div className={styles.box}>
       <div className={styles.position}>
         <ImagePhone className={styles.phone} />
-        {userData ? (
+        {userData && userData.image ? (
           <img src={userData.image} alt="Avatar" className={styles.image} />
         ) : null}
+        <Stack orientation="vertical" gap="16px">
+          {userData && userData.firstName ? (
+            <Text color="black" type="heading" size="m" className={styles.name}>
+              {`${userData.firstName} ${userData.lastName}`}
+            </Text>
+          ) : null}
+          {userData && userData.email ? (
+            <Text color="black" type="body" size="s" className={styles.email}>
+              {userData.email}
+            </Text>
+          ) : null}
+        </Stack>
         {userLinkData &&
           userLinkData
             .slice(0, 5)
