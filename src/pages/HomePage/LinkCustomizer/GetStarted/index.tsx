8974b8bf9dcd4fe7ca8b0035abb9aed5266d8391
Adriana <97173869+a-zajonc@users.noreply.github.com>
2024-01-30 11:52:00 +0100

    Add Mobile Getting Started and NavBar

--- src/pages/HomePage/LinkCustomizer/GetStarted/index.tsx
+++ src/pages/HomePage/LinkCustomizer/GetStarted/index.tsx
@@ -2,17 +2,26 @@ import styles from "./index.module.scss";
 import { Stack } from "../../../../components/Stack";
 import { Text } from "../../../../components/Text";
 import { ReactComponent as ScreenImage } from "../../../../images/illustration-empty.svg";
+import useDeviceType from "../../../../components/useDeviceType";
+import clsx from "clsx";
 
 export function GetStarted() {
+  const { isMobile } = useDeviceType();
+
   return (
     <Stack
       orientation="vertical"
       align="center"
       spacing="center"
-      className={styles.box}
+      className={isMobile ? clsx(styles.box, styles["box-mobile"]) : styles.box}
       gap="40px"
     >
-      <Stack orientation="vertical" className={styles.textcontainer} gap="24px">
+      <ScreenImage className={isMobile ? styles.iconMobile : styles.icon} />
+      <Stack
+        orientation="vertical"
+        className={isMobile ? null : styles.textcontainer}
+        gap="24px"
+      >
         <Text type="heading" size="m">
           Let’s get you started
         </Text>
@@ -22,7 +31,6 @@ export function GetStarted() {
           your profiles with everyone!
         </Text>
       </Stack>
-      <ScreenImage />
     </Stack>
   );
 }
