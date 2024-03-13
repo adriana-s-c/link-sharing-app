765df3a790eaedadcee28e6fadb77304d23d619c
Adriana <97173869+a-zajonc@users.noreply.github.com>
2024-03-13 11:18:38 +0100

    Add Accessiblity to Open and to Close Select

--- src/components/Select/OptionItem/index.tsx
+++ src/components/Select/OptionItem/index.tsx
@@ -9,31 +9,34 @@ type OptionItemProps = {
   onClick: () => void;
 };
 
-export function OptionItem({
-  icon,
-  value,
-  isActive,
-  onClick,
-}: OptionItemProps) {
-  return (
-    <div
-      key={value}
-      onClick={onClick}
-      className={
-        isActive
-          ? `${styles.option} ${styles[`option-isActive`]}`
-          : styles.option
-      }
-    >
-      <Stack gap="12px" align="center">
-        {icon &&
-          React.cloneElement(icon, {
-            className: isActive
-              ? `${styles.icon} ${styles[`icon-isActive`]}`
-              : styles.icon,
-          })}
-        {value}
-      </Stack>
-    </div>
-  );
-}
+export const OptionItem = React.forwardRef<HTMLDivElement, OptionItemProps>(
+  (props, ref) => {
+    const { icon, value, isActive, onClick } = props;
+    return (
+      <div
+        tabIndex={0}
+        ref={ref}
+        role="option"
+        key={value}
+        aria-selected={isActive ? "true" : "false"}
+        onKeyDown={(e) => e.key === "Enter" && onClick()}
+        onClick={onClick}
+        className={
+          isActive
+            ? `${styles.option} ${styles[`option-isActive`]}`
+            : styles.option
+        }
+      >
+        <Stack gap="12px" align="center">
+          {icon &&
+            React.cloneElement(icon, {
+              className: isActive
+                ? `${styles.icon} ${styles[`icon-isActive`]}`
+                : styles.icon,
+            })}
+          {value}
+        </Stack>
+      </div>
+    );
+  }
+);
--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -21,8 +21,26 @@ export function Select({
   onChange,
 }: Props) {
   const [isOpen, setIsOpen] = React.useState(false);
-
   const selectRef = React.useRef<HTMLDivElement>(null);
+  const [focusedIndex, setFocusedIndex] = React.useState<number | null>(0);
+  const optionRefs = React.useRef<(HTMLDivElement | null)[]>([]);
+
+  React.useEffect(() => {
+    if (isOpen && focusedIndex !== null && optionRefs.current[focusedIndex]) {
+      optionRefs.current[focusedIndex]?.focus();
+    }
+  }, [isOpen, focusedIndex]);
+
+  React.useEffect(() => {
+    if (isOpen) {
+      const currentSelectedIndex = options.findIndex(
+        (option) => option.value === selectedOption.value
+      );
+      setFocusedIndex(currentSelectedIndex >= 0 ? currentSelectedIndex : 0);
+    } else {
+      setFocusedIndex(null);
+    }
+  }, [isOpen, options, selectedOption.value]);
 
   React.useEffect(() => {
     function handleClickOutside(event: MouseEvent) {
@@ -42,6 +60,51 @@ export function Select({
     setIsOpen(!isOpen);
   };
 
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (!isOpen) {
+      // Open the select with any keyDown if it's not open
+      if (
+        e.key === "Enter" ||
+        e.key === " " ||
+        e.key === "ArrowDown" ||
+        e.key === "ArrowUp"
+      ) {
+        e.preventDefault();
+        setIsOpen(true);
+      }
+      return;
+    }
+    switch (e.key) {
+      case "ArrowDown":
+        e.preventDefault();
+        setFocusedIndex((prevIndex) =>
+          prevIndex !== null ? (prevIndex + 1) % options.length : 0
+        );
+        break;
+      case "ArrowUp":
+        e.preventDefault();
+        setFocusedIndex((prevIndex) =>
+          prevIndex !== null
+            ? (prevIndex - 1 + options.length) % options.length
+            : options.length - 1
+        );
+        break;
+      case "Enter":
+      case " ":
+        e.preventDefault();
+        if (focusedIndex !== null && setSelectedOption) {
+          setSelectedOption(options[focusedIndex]);
+          setIsOpen(false);
+        }
+        break;
+      case "Escape":
+        setIsOpen(false);
+        break;
+      default:
+        break;
+    }
+  };
+
   const handleOptionClick = (option: Option) => {
     if (setSelectedOption) {
       setSelectedOption(option);
@@ -50,14 +113,27 @@ export function Select({
     if (onChange) {
       onChange(option);
     }
-
     setIsOpen(false);
   };
 
   const isSelectedOption = (option: string) => option === selectedOption.value;
 
+  if (optionRefs.current.length !== options.length) {
+    optionRefs.current = Array(options.length).fill(null);
+  }
+
   return (
-    <div className={`${styles.relative} custom-select`} ref={selectRef}>
+    <div
+      className={`${styles.relative} custom-select`}
+      ref={selectRef}
+      tabIndex={0}
+      role="combobox"
+      aria-haspopup="listbox"
+      aria-expanded={isOpen ? "true" : "false"}
+      aria-owns="optionsList"
+      aria-controls="optionsList"
+      onKeyDown={handleKeyDown}
+    >
       <Stack
         className={
           isOpen ? ` ${styles.box} ${styles[`box-isOpen`]}` : styles.box
@@ -80,9 +156,10 @@ export function Select({
           className={`${styles.options} options-div`}
           gap="12px"
         >
-          {options.map((option) => (
+          {options.map((option, index) => (
             <OptionItem
               key={option.value}
+              ref={(el) => (optionRefs.current[index] = el)}
               icon={GetIcon({ name: option.value, color: "white" })}
               value={option.value}
               isActive={isSelectedOption(option.value)}
